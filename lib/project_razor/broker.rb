00a5681c4a9ddd598691784fbd483dab6e7fbc21
Sam Dunster <sdunster@uow.edu.au>
2012-12-06 20:41:43 +1100

    Added puppetenterprise broker
    This broker can download a puppet enterprise package (the second item
    in the servers array will be used as the URL) and install by generating
    a puppet installer answer file with the appropriate puppet server.
    Cert name will be `hostname -f`.

--- lib/project_razor/broker.rb
+++ lib/project_razor/broker.rb
@@ -1,6 +1,7 @@
 require "project_razor/broker/base"
 require "project_razor/broker/puppet"
 require "project_razor/broker/chef"
+require "project_razor/broker/puppetenterprise"
 
 module Broker
 
---
+++ lib/project_razor/broker/puppetenterprise.rb
@@ -0,0 +1,97 @@
+# Our puppet plugin which contains the agent & device proxy classes used for hand off
+
+# TODO - Make broker properties open rather than rigid
+require "erb"
+require "net/ssh"
+
+# Root namespace for ProjectRazor
+module ProjectRazor::BrokerPlugin
+
+  # Root namespace for Puppet Broker plugin defined in ProjectRazor for node handoff
+  class PuppetEnterprise < ProjectRazor::BrokerPlugin::Base
+    include(ProjectRazor::Logging)
+
+    def initialize(hash)
+      super(hash)
+
+      @plugin = :puppetenterprise
+      @description = "PuppetLabs Puppet Enterprise"
+      @hidden = false
+      from_hash(hash) if hash
+    end
+
+    def agent_hand_off(options = {})
+      @options = options
+      @options[:server] = @servers.first
+      @options[:version] = @broker_version
+      @options[:package_url] = @servers[1] # second server will be the package URL
+      return false unless validate_options(@options, [:username, :password, :server, :ca_server, :puppetagent_certname, :ipaddress, :package_url])
+      @puppet_script = compile_template
+      init_agent(options)
+    end
+
+    def proxy_hand_off(options = {})
+      res = "
+      @@vc_host { '#{options[:ipaddress]}':
+        ensure   => 'present',
+        username => '#{options[:username]}',
+        password => '#{options[:password]}',
+        tag      => '#{options[:vcenter_name]}',
+      }
+      "
+      system("puppet apply --certname=#{options[:hostname]} --report -e \"#{res}\"")
+      :broker_success
+    end
+
+    # Method call for validating that a Broker instance successfully received the node
+    def validate_hand_off(options = {})
+      # Return true until we add validation
+      true
+    end
+
+    def init_agent(options={})
+      @run_script_str = ""
+      begin
+        Net::SSH.start(options[:ipaddress], options[:username], { :password => options[:password], :user_known_hosts_file => '/dev/null'} ) do |session|
+          logger.debug "Copy: #{session.exec! "echo \"#{@puppet_script}\" > /tmp/puppet_init.sh" }"
+          logger.debug "Chmod: #{session.exec! "chmod +x /tmp/puppet_init.sh"}"
+          @run_script_str << session.exec!("bash /tmp/puppet_init.sh |& tee /tmp/puppet_init.out")
+          @run_script_str.split("\n").each do |line|
+            logger.debug "puppet script: #{line}"
+          end
+        end
+      rescue => e
+        logger.error "puppet agent error: #{e}"
+        return :broker_fail
+      end
+      # set return to fail by default
+      ret = :broker_fail
+      # set to wait
+      ret = :broker_wait if @run_script_str.include?("Exiting; no certificate found and waitforcert is disabled")
+      # set to success (this meant autosign was likely on)
+      ret = :broker_success if @run_script_str.include?("Finished catalog run")
+      ret
+    end
+
+
+    def compile_template
+      logger.debug "Compiling template"
+      install_script = File.join(File.dirname(__FILE__), "puppetenterprise/agent_install.erb")
+      contents = ERB.new(File.read(install_script)).result(binding)
+      logger.debug("Compiled installation script:")
+      logger.error install_script
+      #contents.split("\n").each {|x| logger.error x}
+      contents
+    end
+
+    def validate_options(options, req_list)
+      missing_opts = req_list.select do |opt|
+        options[opt] == nil
+      end
+      unless missing_opts.empty?
+        false
+      end
+      true
+    end
+  end
+end
